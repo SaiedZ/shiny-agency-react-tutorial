6f8638924abe5028bef848423e861c0bb5f041e1
Deias <saied_am@hotmail.com>
2022-09-07 10:29:52 +0100

    used state and props to style components / styled

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
+
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+const StyledLink = styled(Link)`
+    padding: 15px;
+    color: #8186a0;
+    text-decoration: none;
+    font-size: 18px;
+    ${(props) =>
+        props.$isFullLink &&
+        `color: white; border-radius: 30px; background-color: #5843E4;`}
+`
 
 function Header() {
     return (
         <nav>
-            <Link to="/">Accueil</Link>
-            <Link to="/survey/1">Questionnaire</Link>
-            <Link to="/freelances">Freelances</Link>
+            <StyledLink to="/">Accueil</StyledLink>
+            <StyledLink to="/survey/1" $isFullLink>
+                Faire le test
+            </StyledLink>
+            <StyledLink to="/freelances">Freelances</StyledLink>
         </nav>
     )
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,15 +1,36 @@
 import React from 'react'
+import { useState } from 'react'
+
+import styled from 'styled-components'
+
+const HomeContainer = styled.div`
+    width: 100%;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+`
+
+const Balloon = styled.div`
+    width: 100px;
+    height: 100px;
+    background-color: gold;
+    border-radius: 50px;
+    transform: scale(${(props) => props.size});
+`
 
 function Home() {
+    const [size, setSize] = useState(1)
+
     return (
-        <div>
-            <h1>
+        <HomeContainer>
+            <h1 onClick={() => setSize(size + 0.1)}>
                 Page d'accueil{' '}
                 <span role="img" aria-label="desc">
                     🏡
                 </span>
             </h1>
-        </div>
+            <Balloon size={size} />
+        </HomeContainer>
     )
 }
 
